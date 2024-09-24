d7e9397e16274a978bdd02458b4549024981225c
Geetika Kanwar <133354881+Geetika2282@users.noreply.github.com>
2024-09-24 16:02:49 +0530

    Add files via upload

---
+++ mathFunc.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+
+public class mathFunc {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-4.24));
+        System.out.println(Math.abs(-3.467));
+        System.out.println(Math.addExact(12, 11));
+        System.out.println(Math.ceil(12.35));
+        System.out.println(Math.floor(12.35));
+
+    }
+}
