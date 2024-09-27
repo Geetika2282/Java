9d24094b0d6f2bcaf86a0921efa91d2e0a65cadc
Geetika Kanwar <133354881+Geetika2282@users.noreply.github.com>
2024-09-27 23:24:39 +0530

    Update StrBuilder.java

--- StrBuilder.java
+++ StrBuilder.java
@@ -1,10 +1,23 @@
 public class StrBuilder {
+    public boolean isPal(String txt){
+        StringBuilder s = new StringBuilder(txt);
+        s.reverse();
+        String reversed = s.toString();
+        return txt.equals(reversed);
+    }
     public static void main(String[] args) {
+        StrBuilder s1 = new StrBuilder();
+
+//        Using StringBuilder so can edits Strings (mutable)
+     System.out.println(s1.isPal("arbra"));
      StringBuilder sb = new StringBuilder();
      for(int i=0; i<26; i++){
          char ch = (char) ('a'+i);
          sb.append(ch);
      }
         System.out.println(sb.toString());
+        sb.reverse();
+        System.out.println(sb.toString());
     }
+
 }
