cc980fa848557806856b9906228794f03911e953
Geetika <133354881+Geetika2282@users.noreply.github.com>
2024-09-29 18:12:33 +0530

    Add files via upload

---
+++ AccessMod.java
@@ -0,0 +1,11 @@
+import Shapes.AccessModBase;
+
+public class AccessMod extends AccessModBase {
+    public AccessMod(int num, String name) {
+        super(num, name);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
